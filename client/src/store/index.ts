b72697d8859c469087aa179f822d6988dd0dc446
David Schwarz <schwarz.david10@gmail.com>
2018-10-19 23:27:33 +0200

    Store started

---
+++ client/src/store/index.ts
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import Vuex, {ModuleTree} from "vuex";
+import {IRootState} from "./typings";
+
+Vue.use(Vuex);
+
+const VERSION = '1.0.0';
+const state: IRootState = {
+	version: VERSION
+};
+const modules: ModuleTree<IRootState> = {
+	
+};
